257b3fc71ea690be654d2ecbaca1db32edc573d6
Burak Örkmez <burakorkmezz@gmail.com>
2023-07-16 23:41:00 +0200

    Home page content added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 function App() {
 	return (
 		<Container maxW='620px'>
 			<Header />
 			<Routes>
+				{/* This route added after the tutorial. */}
+				<Route path='/' element={<HomePage />} />
+
 				<Route path='/:username' element={<UserPage />} />
 				<Route path='/:username/post/:pid' element={<PostPage />} />
 			</Routes>
