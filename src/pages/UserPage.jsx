65ff2cd8272c5291a1eb6aaf0c1e612877df6e38
Burak Örkmez <burakorkmezz@gmail.com>
2023-07-17 03:04:04 +0200

    Readme added

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -6,8 +6,13 @@ const UserPage = () => {
 		<>
 			<UserHeader />
 			<UserPost likes={1200} replies={481} postImg='/post1.png' postTitle="Let's talk about threads." />
-			<UserPost likes={451} replies={12} postImg='/post2.png' postTitle='Nice tutorial' />
-			<UserPost likes={321} replies={989} postImg='/post3.png' postTitle='I love this guy.' />
+			<UserPost likes={451} replies={12} postImg='/post2.png' postTitle='Nice tutorial. Highly recommended.' />
+			<UserPost
+				likes={6721}
+				replies={989}
+				postImg='/post3.png'
+				postTitle="I love this guy and can't wait to see him in cage. 💪"
+			/>
 			<UserPost likes={212} replies={56} postTitle='This is my first thread.' />
 		</>
 	);
